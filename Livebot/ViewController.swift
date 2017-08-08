df52e1c1bc3fe38fe0c1655fbcdc922926c7a26c
zuchie <zuchie@gmail.com>
2017-08-07 23:20:27 -0700

    Initial Commit

---
+++ Livebot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Livebot
+//
+//  Created by Zhe Cui on 8/7/17.
+//  Copyright © 2017 Zhe Cui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
