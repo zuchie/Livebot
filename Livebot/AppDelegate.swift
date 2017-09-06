5dba62d80fda78fe3447ca092b392bb6b61f07f4
zuchie <zuchie@gmail.com>
2017-09-05 17:14:48 -0700

    Initial commit.

--- Livebot/AppDelegate.swift
+++ Livebot/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        AI.configure(clientAccessToken: "1d1ab7fd86f949bf833c7c81f661dbd3")
+        
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,19 @@
+use_frameworks!
+platform :ios, '10.0'
+
+pod 'AI', '~> 0.0.8'
+pod 'RxSwift', '~> 3.6'
+pod 'RxCocoa', '~> 3.6'
+pod 'RxRealm', '~> 0.6'
+pod 'SwiftyJSON', '~> 3.1.4'
+pod 'RxAlamofire', '~> 3.0.3'
+  
+target 'Livebot' do
+  pod 'RxDataSources', '~> 1.0.3'
+  pod 'Unbox', '~> 2.4.0'
+  pod 'Kingfisher', '~> 3.9.1'
+end
+
+target 'LivebotTests' do
+  pod 'RxNimble', '~> 3.0.0'
+end
